e9487cdfa3186b1cc4490bcacfe26d9d942822d2
Asif Fasih <aafasih@gmail.com>
2023-09-20 13:41:14 -0500

    updating EmojiIdentification to create an isEmoji method and add a variable that is not an emoji

--- src/com/fun/java/emoji/EmojiIdentification.java
+++ src/com/fun/java/emoji/EmojiIdentification.java
@@ -2,10 +2,16 @@ package com.fun.java.emoji;
 
 public class EmojiIdentification {
     public static void main(String[] args) {
-        var codePoint = Character.codePointAt("\uD83E\uDD23", 0);
-        var isEmoji = Character.isEmoji(codePoint);
+        var emoji = "\uD83E\uDD23";
+        var notEmoji = "ↂ";
 
-        //this should print "true" as it detects the value is an emoji
-        System.out.println("Is this an emoji? " + isEmoji);
+        //the symbol assigned to the notEmoji variable is not an emoji
+        System.out.println("Is this an emoji? " + isEmoji(emoji));
+        System.out.println("Is this symbol an emoji? " + isEmoji(notEmoji));
+
+    }
+
+    protected static boolean isEmoji (String symbol) {
+     return Character.isEmoji(Character.codePointAt(symbol, 0));
     }
 }
